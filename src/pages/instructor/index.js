80d7e23286ec41ebf1e028eac721ca4943da2948
rstr26 <tabernillad3rd@gmail.com>
2023-07-04 22:37:15 +0800

    refactored auth, wrapped protected routes in auth component

--- src/pages/instructor/index.js
+++ src/pages/instructor/index.js
@@ -5,56 +5,42 @@ import Cookies from 'js-cookie'
 import React, { useEffect } from 'react'
 import { Button, Container } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
+import RequireAuth from '@/components/RequireAuth'
 
 const index = () => {
   const router = useRouter()
 
-  // Authorize access to this page
-  useEffect(() => {
-    Authorize(Cookies.get('accessToken'))
-    .then((result) => {
-      console.log(result)
-      if(result === 'unauthorized' || result === 'forbidden'){
-        Alert('info', 'Unauthorized Access', `Your access is ${result}, redirecting to log in page...`, 3000)
-        Cookies.remove('accessToken')
-        router.replace('/')
-      }
-      else if(result.role !== 'teacher' && router.pathname.includes('instructor')){
-        Alert('info', 'Access Denied', 'You do not have access to this page', 3000)
-        router.replace('../')
-      }
-    })
-  }, [])
-
   return (
-    <Sidebar type='instructor'>
-      <Container>
-        {/* Stats Grid */}
-        <Grid container columns={3} columnSpacing={2} rowSpacing={1}>
-          <Grid item xs={3} md={1}>
-            <Paper elevation={3} sx={{padding: '10px'}}>
-              <h3>Subject</h3>
-            </Paper>
+    <RequireAuth>
+      <Sidebar type='instructor'>
+        <Container>
+          {/* Stats Grid */}
+          <Grid container columns={3} columnSpacing={2} rowSpacing={1}>
+            <Grid item xs={3} md={1}>
+              <Paper elevation={3} sx={{padding: '10px'}}>
+                <h3>Subject</h3>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={3} md={1}>
+              <Paper elevation={3} sx={{padding: '10px'}}>
+                <h3>Students</h3>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={3} md={1}>
+              <Paper elevation={3} sx={{padding: '10px'}}>
+                <h3>Pending for Enrollment</h3>
+              </Paper>
+            </Grid>
           </Grid>
 
-          <Grid item xs={3} md={1}>
-            <Paper elevation={3} sx={{padding: '10px'}}>
-              <h3>Students</h3>
-            </Paper>
-          </Grid>
-
-          <Grid item xs={3} md={1}>
-            <Paper elevation={3} sx={{padding: '10px'}}>
-              <h3>Pending for Enrollment</h3>
-            </Paper>
-          </Grid>
-        </Grid>
-
-        {/* <Button onClick={() => Cookies.set('key', 'Cookie 1')}>Set Cookie</Button>
-        <Button onClick={() => console.log(Cookies.get('accessToken'))}>Retrieve Cookie</Button>
-        <Button onClick={() => Cookies.remove('accessToken')}>Remove Cookie</Button> */}
-      </Container>
-    </Sidebar>
+          {/* <Button onClick={() => Cookies.set('key', 'Cookie 1')}>Set Cookie</Button>
+          <Button onClick={() => console.log(Cookies.get('accessToken'))}>Retrieve Cookie</Button>
+          <Button onClick={() => Cookies.remove('accessToken')}>Remove Cookie</Button> */}
+        </Container>
+      </Sidebar>
+    </RequireAuth>
   )
 }
 
--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -3,31 +3,17 @@ import React, { useEffect } from 'react'
 import { Authorize, Alert } from '@/components/shared'
 import { useRouter } from 'next/router'
 import Cookies from 'js-cookie'
+import RequireAuth from '@/components/RequireAuth'
 
 const index = () => {
   const router = useRouter()
 
-  useEffect(() => {
-    Authorize(Cookies.get('accessToken'))
-    .then((result) => {
-      console.log(result)
-      if(result === 'unauthorized' || result === 'forbidden'){
-        Alert('info', 'Unauthorized Access', `Your access is ${result}, redirecting to log in page...`, 3000)
-        Cookies.remove('accessToken')
-        router.replace('/')
-      }
-      else if(result.role !== 'student' && router.pathname.includes('student')){
-        Alert('info', 'Access Denied', 'You do not have access to this page', 3000)
-        router.replace('../')
-      }
-    })
-  }, [])
   return (
-    <React.Fragment>
+    <RequireAuth>
       <Sidebar type='student'>
         <h1>Hello Im the Student Page</h1>
       </Sidebar>
-    </React.Fragment>
+    </RequireAuth>
   )
 }
 
